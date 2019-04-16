9d01906748f748ec25bd3a616d4699501b057174
didiloki <eiweala@live.com>
2019-04-16 14:30:31 +0300

    add controller logic

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,6 @@
 class ArtistsController < ApplicationController
   def index
+    @artist = Artist.all
   end
 
   def new
@@ -9,5 +10,11 @@ class ArtistsController < ApplicationController
   end
 
   def show
+    @artist = Artist.find(params[:id])
+  end
+
+  private
+  def artist_params
+    params.require(:artist).permit(:name)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+artist = Artist.create([{name: 'Drake'}])
+
+album = Album.create([{name: 'Back to Back', cover: 'https://static.hiphopdx.com/2015/07/Screen-Shot-2015-07-29-at-5.31.52-AM.png', year: 2007, artist_id: 1}])
+
+songs = Song.create([{name: 'Back to Back', url: 'http://naijaplayhiphop.com/wp-content/uploads/music/Drake-Back-To-Back-Freestyle_NaijaPlayhiphop.com_.mp3', album_id: 1 }])
