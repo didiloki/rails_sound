933738f22dbdd2dfc1a5dcc3e8943a31f5f02668
alanoud-GA <alanoud.alrasheed@generalassemb.ly>
2019-04-16 14:07:33 +0300

    created controllers/views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'artists/index'
+  get 'artists/new'
+  get 'artists/edit'
+  get 'artists/show'
+  get 'songs/index'
+  get 'songs/new'
+  get 'songs/edit'
+  get 'songs/show'
+  get 'albums/index'
+  get 'albums/new'
+  get 'albums/edit'
+  get 'albums/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
