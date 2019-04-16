a89896505349457ef82e80cef85a368d24c82187
didiloki <eiweala@live.com>
2019-04-16 14:05:38 +0300

    added relationships

--- db/migrate/20190416084828_create_albums.rb
+++ db/migrate/20190416084828_create_albums.rb
@@ -3,6 +3,7 @@ class CreateAlbums < ActiveRecord::Migration[5.2]
     create_table :albums do |t|
       t.string :name
       t.string :cover
+      t.string :year
       t.integer :artist_id
 
       t.timestamps
--- db/migrate/20190416085849_create_songs.rb
+++ db/migrate/20190416085849_create_songs.rb
@@ -2,7 +2,8 @@ class CreateSongs < ActiveRecord::Migration[5.2]
   def change
     create_table :songs do |t|
       t.string :name
-
+      t.integer :album_id
+      
       t.timestamps
     end
   end
